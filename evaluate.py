1d3f50019ff2634687c3a1d7b60493ab6469e5b4
Nora Tang <jingyu_tang@brown.edu>
2024-04-18 11:49:48 -0400

    reconstructed parser

--- evaluate.py
+++ evaluate.py
@@ -1,48 +1,8 @@
-import argparse
-import numpy as np
-
-IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
-
-MODEL = 'DeepLab'
-DATA_DIRECTORY = './dataset/VOC2012'
-DATA_LIST_PATH = './dataset/voc_list/val.txt'
-IGNORE_LABEL = 255
-NUM_CLASSES = 21
-NUM_STEPS = 1449 # Number of images in the validation set.
-RESTORE_FROM = 'http://vllab1.ucmerced.edu/~whung/adv-semi-seg/AdvSemiSegVOC0.125-8d75b3f1.pth'
-PRETRAINED_MODEL = None
-SAVE_DIRECTORY = 'results'
-
-def get_arguments():
-    """Parse all the arguments provided from the CLI.
-
-    Returns:
-      A list of parsed arguments.
-    """
-    parser = argparse.ArgumentParser(description="VOC evaluation script")
-    parser.add_argument("--model", type=str, default=MODEL,
-                        help="available options : DeepLab/DRN")
-    parser.add_argument("--data-dir", type=str, default=DATA_DIRECTORY,
-                        help="Path to the directory containing the PASCAL VOC dataset.")
-    parser.add_argument("--data-list", type=str, default=DATA_LIST_PATH,
-                        help="Path to the file listing the images in the dataset.")
-    parser.add_argument("--ignore-label", type=int, default=IGNORE_LABEL,
-                        help="The index of the label to ignore during the training.")
-    parser.add_argument("--num-classes", type=int, default=NUM_CLASSES,
-                        help="Number of classes to predict (including background).")
-    parser.add_argument("--restore-from", type=str, default=RESTORE_FROM,
-                        help="Where restore model parameters from.")
-    parser.add_argument("--pretrained-model", type=str, default=PRETRAINED_MODEL,
-                        help="Where restore model parameters from.")
-    parser.add_argument("--save-dir", type=str, default=SAVE_DIRECTORY,
-                        help="Directory to store results")
-    parser.add_argument("--gpu", type=int, default=0,
-                        help="choose gpu device.")
-    return parser.parse_args()
+from parser.parser import EvalArgParser
 
 def main(args):
-    pass
+    print(args)
 
 if __name__ == '__main__':
-    args = get_arguments()
-    main(args)
+    eval_parser = EvalArgParser()
+    main(eval_parser.get_arguments())
